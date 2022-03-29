7d5a3dc8d3a444566feed13f07f2ef6cd852e2e1
yanru.cho <yanru122579@gmail.com>
2022-03-29 21:31:27 +0800

    bg img

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        red:`#FF7979`,
+        green:`#38CC8B`,
+        blue:{
+          DEFAULT:`#5E54A4`,
+          dark:`#3D3B4B`
+        }
+      }
+    },
   },
   plugins: [],
 }
